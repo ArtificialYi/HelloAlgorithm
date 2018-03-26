9a0a84cdfd06be59e41a3f06bef9acce34fc95ae
huangyi <huangyi@youzan.com>
2018-03-26 13:33:05 +0800

    test: 初始化算法项目

---
+++ HelloAlgorithm/HelloAlgorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloAlgorithm
+//
+//  Created by 黄毅 on 2018/3/26.
+//  Copyright © 2018年 hy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
