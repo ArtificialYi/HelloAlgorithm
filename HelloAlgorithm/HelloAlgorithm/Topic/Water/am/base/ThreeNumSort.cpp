209375945c0a9970b65ef410f725cabb6769e9ae
huangyi <huangyi@youzan.com>
2018-04-07 17:35:03 +0800

    test: 阿敏小朋友的三数排序

---
+++ HelloAlgorithm/HelloAlgorithm/Topic/Water/am/base/ThreeNumSort.cpp
@@ -0,0 +1,13 @@
+//
+//  ThreeNumSort.cpp
+//  HelloAlgorithm
+//
+//  Created by 黄毅 on 2018/4/7.
+//  Copyright © 2018年 hy. All rights reserved.
+//
+
+#include "ThreeNumSort.hpp"
+
+int ThreeNumSort::main() {
+    return 0;
+}
---
+++ HelloAlgorithm/HelloAlgorithm/Topic/Water/am/base/ThreeNumSort.hpp
@@ -0,0 +1,19 @@
+//
+//  ThreeNumSort.hpp
+//  HelloAlgorithm
+//
+//  Created by 黄毅 on 2018/4/7.
+//  Copyright © 2018年 hy. All rights reserved.
+//
+
+#ifndef ThreeNumSort_hpp
+#define ThreeNumSort_hpp
+
+#include <stdio.h>
+
+class ThreeNumSort {
+public:
+    int main();
+};
+
+#endif /* ThreeNumSort_hpp */
